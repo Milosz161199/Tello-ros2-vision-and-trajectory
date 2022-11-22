f461c27360688f312d6de3f5dc0f239a30f31484
Albert Polak <lugii1999@gmail.com>
2022-11-22 20:39:22 +0000

    warp perspective

--- paper_detection/paper_detection/detect_server.py
+++ paper_detection/paper_detection/detect_server.py
@@ -10,6 +10,7 @@ import time
 import rclpy # Python Client Library for ROS 2
 from sensor_msgs.msg import Image # Image is the message type
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
+from imutils.perspective import order_points
 
 
 class DetectActionServer(Node):
@@ -21,6 +22,7 @@ class DetectActionServer(Node):
             Detect,
             'Detect',
             self.execute_callback)
+        
 
     def execute_callback(self, goal_handle):
         self.get_logger().info('Executing goal...')
@@ -35,6 +37,21 @@ class DetectActionServer(Node):
         result.image = roi
         return result
 
+    def perspective_transformation(self, img, box):
+        box = np.array(box, dtype="int")
+        src_pts = order_points(box)
+
+        # use Euclidean distance to get width & height
+        width = int(np.linalg.norm(src_pts[0] - src_pts[1]))
+        height = int(np.linalg.norm(src_pts[0] - src_pts[3]))
+
+        dst_pts = np.array([[0,0], [width,0], [width,height], [0,height]], dtype=np.float32)
+
+        M = cv.getPerspectiveTransform(src_pts, dst_pts)
+        warped_img = cv.warpPerspective(img, M, (width, height))
+
+        return warped_img
+        
     def detect_paper(self):
         br = CvBridge()
         result = Image()
@@ -42,7 +59,7 @@ class DetectActionServer(Node):
         # from itertools import combinations
 
         # Load the webcam
-        # cap = cv.VideoCapture(0)
+        cap = cv.VideoCapture(0)
 
         # Add sliders for canny parameters
         # cv.namedWindow("Trackbars")
@@ -61,10 +78,10 @@ class DetectActionServer(Node):
             # max_threshold = cv.getTrackbarPos("Max Threshold", "Trackbars")
 
             # Read the webcam
-            # _, img = cap.read()
-            img = cv.imread("room_with_grid2.png")
+            _, img = cap.read()
+            # img = cv.imread("room_with_grid2.png")
             
-            img = cv.resize(img, (0, 0), fx=0.2, fy=0.2)
+            # img = cv.resize(img, (0, 0), fx=0.4, fy=0.4)
             # Convert to grayscale
             gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
@@ -72,11 +89,16 @@ class DetectActionServer(Node):
 
             blur = cv.morphologyEx(blur, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)), iterations=2)
 
-            canny = cv.Canny(blur, 1, 84)
+            # canny = cv.Canny(blur, 1, 84)
+            canny = cv.Canny(blur, 30, 50)
             canny = cv.morphologyEx(canny, cv.MORPH_CLOSE, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)), iterations=2)
             
             contours, hierarchy = cv.findContours(canny, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-            biggest = []
+
+            biggest = None
+            roi = None
+            # biggest = []
+            
             max_area = 0
             for contour in contours:
                 area = cv.contourArea(contour)
@@ -85,46 +107,20 @@ class DetectActionServer(Node):
                     peri = cv.arcLength(contour, True)
                     approx = cv.approxPolyDP(contour, 0.02 * peri, True)
                     if area > max_area and len(approx) == 4:
-                        biggest = []
-                        biggest.append(approx)
-                        print(approx)
+                        # biggest = []
+                        # biggest.append(approx)
+                        warp_box = approx.reshape(4,2)
+                        biggest = contour
+                        # print(approx)
                         max_area = area
             
-            cv.drawContours(img, biggest, -1, (0, 255, 0), 2)
-
-            # cv.imshow("Blur", blur)
-            # cv.imshow("Canny", canny)
-            # cv.imshow('img', img)
-
-            thresh = cv.threshold(gray, 1, 255, cv.THRESH_BINARY_INV)[1]
-            thresh = cv.morphologyEx(thresh, cv.MORPH_OPEN, cv.getStructuringElement(cv.MORPH_RECT, (5, 5)), iterations=1)
             
-            dst = cv.cornerHarris(thresh,4,3,0.04)
-
-            # x, y = img[dst>0.01*dst.max()]
-            coordinates = np.where(dst > 0.01 * dst.max())
-
-
-            # draw corners
-            corners = np.array(list(zip(coordinates[1], coordinates[0])))   
-
-            # match two points in corners
-            # if they are not on an approximately horizontal or vertical line, draw a line between them
-
-            # comb = list(combinations(corners, 2))
-            # for i in comb:
-            #     if abs(i[0][0] - i[1][0]) < 10 or abs(i[0][1] - i[1][1]) < 10:
-            #         cv.line(img, i[0], i[1], (0, 0, 255), 2)
-
-            
-            for corner in corners:
-                cv.circle(img, tuple(corner), 5, (0, 0, 255), -1)
-
-
-            rect = cv.minAreaRect(corners)
-            box = cv.boxPoints(rect)
-            box = np.int0(box)
-            cv.drawContours(img, [box], 0, (0, 0, 255), 2)
+            if biggest is not None:
+                # cv.drawContours(img, biggest, -1, (0, 255, 0), 2)
+                rect = cv.minAreaRect(biggest)
+                box = cv.boxPoints(rect)
+                box = np.int0(box)
+                cv.drawContours(img, [box], 0, (0, 0, 255), 2)
 
             # calculate elapsed time
             elapsed = time.time() - start
@@ -135,31 +131,24 @@ class DetectActionServer(Node):
                 difference = cv.matchShapes(box, previous_contour, 2, 0.0)
                 print(difference)
                 # reset timer
-                if difference < 0.1:
+                if difference < 0.1 and biggest is not None:
                     print("Paper detected")
                     x, y, w, h = cv.boundingRect(box)
                     roi = img[y:y+h, x:x+w]
+                    roi = self.perspective_transformation(img, warp_box)
+                    cv.imwrite('roi.png', roi)
                     result = br.cv2_to_imgmsg(roi)
                     return result
                 start = time.time()
                 # set previous contour to current contour
                 previous_contour = box
 
-            if previous_contour is None:
+            if previous_contour is None and biggest is not None:
                 previous_contour = box
 
-            
-
-
-
-            # cv.imshow("Corners", img)
+            cv.imshow("Corners", img)
             # cv.imshow("ROI", roi)
             # cv.imshow("Thresh", thresh)
-
-
-
-            
-
 
             # Stop if escape key is pressed
             k = cv.waitKey(30) & 0xff
@@ -169,10 +158,10 @@ class DetectActionServer(Node):
 
 
         # Release the webcam
-        # cap.release()
+        cap.release()
 
         # Close all windows
-        # cv.destroyAllWindows()
+        cv.destroyAllWindows()
 
 
 
