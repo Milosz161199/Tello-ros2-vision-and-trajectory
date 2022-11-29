14bc6fcc1f868f3e5b2ebdd05264f0df8584df6f
Milosz161199 <miloszp@op.pl>
2022-11-29 11:46:23 +0000

    Clenup code

--- paper_detection/setup.py
+++ paper_detection/setup.py
@@ -18,8 +18,5 @@ setup(
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
-    entry_points={
-        'console_scripts': ['img_publisher = cv_basics.webcam_pub:main',
-        ],
-    },
+    entry_points={},
 )
