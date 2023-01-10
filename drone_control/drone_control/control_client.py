e350fdad75aa575832144061f3ff726c84ace178
Kwach00 <krzysiek.kwapisz@gmail.com>
2023-01-10 23:02:36 +0000

    feat: drone control

--- drone_control/drone_control/control_client.py
+++ drone_control/drone_control/control_client.py
@@ -18,30 +18,33 @@ class ControlActionClient(Node):
 
     def __init__(self):
         super().__init__('control_action_client')
-        self._action_client = ActionClient(
-            self,
-            Detect,
-            'Detect')
         
-        # self._optitrack_subscriber = self.create_subscription(
-        #     String,
-        #     'optitrack_topic',
-        #     self.optitrack_callback,
-        #     10)
+        self.control_method = False
+        self.rc_srv_name = "/tello_action"
+        self.topic_cmd_name = "/cmd_vel"
         
-        self._republisher_subscriber = self.create_subscription(
-            Odometry,
-            '/repeater/tello_1/pose/info',
-            self.republisher_callback,
-            10)
+        self.vel_lin = 0.1
+        self.vel_ang = 0.1
+        self.max_vel_lin = 20.0
+        self.max_vel_ang = 40.0
+        self.lin_offset = 0.1
+        self.ang_offset = 0.1
         
-        self._call_service = self.create_client(
-            TelloAction,
-            "/tello_action",
-        )
+        self.curr_measured_x = 0
+        self.curr_measured_y = 0
+        self.curr_measured_z = 0
+        self.curr_x = 0
+        self.curr_y = 0
+        self.curr_z = 0
+        self.dest_x = 0
+        self.dest_y = 0
+        self.dest_z = 0
         
-        self._drone_cmd_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
+        self.reached_x = False
+        self.reached_y = False
+        self.reached_z = False
         
+        self.new_cmd = False
         self.msg_twist = Twist()
         
         self.current_time = 0
@@ -72,16 +75,57 @@ class ControlActionClient(Node):
         self.x_arr = list()
         self.y_arr = list()
         self.z_arr = list()
+        self.number_of_points = 0
+        self.curr_point = 0
+        self.start_control = False
         
-        # Just for tests
-        # self.x_arr = [1, 0, 2, 3]
-        # self.y_arr = [0, 0, 0, 0]
-        # self.z_arr = [1, 1, 1, 1]
-        # self.y_arr = [0, 1, 2, 3]
-        # self.z_arr = [1, 1, 2, 1]
-        self.number_of_points = 4
+        self._action_client = ActionClient(
+            self,
+            Detect,
+            'Detect')
         
+        # self._sub_optitrack = self.create_subscription(
+        #     String,
+        #     'optitrack_topic',
+        #     self.optitrack_callback,
+        #     10)
+        
+        self._sub_republisher = self.create_subscription(
+            Odometry,
+            '/repeater/tello_1/pose/info',
+            self.republisher_callback,
+            10)
+        
+        self._client_rc = self.create_client(
+            TelloAction,
+            self.rc_srv_name)
+        
+        while not self._client_rc.wait_for_service(timeout_sec=2.0):
+            self.get_logger().error("Rc service not available - waiting for service")
+            
+        self.response_rc = self.create_subscription(
+            TelloResponse,
+            "tello_response",
+            self.response_callback,
+            10)
+        
+        self.get_logger().info("Control node has been started")
+        
+        self._client_rc.call_async(TelloAction.Request(cmd="takeoff"))
 
+        self.get_logger().info("Drone took off")
+        
+        self._pub_cmd_vel = self.create_publisher(Twist, self.topic_cmd_name, 10)
+        
+        self.timer_period = 1/10  # 10Hz
+        self._timer = self.create_timer(self.timer_period, self.timer_callback)
+        
+        self.control_timer_period = 1 / 250  # 250Hz
+        self._control_timer = self.create_timer(self.control_timer_period, self.control_timer_callback)
+        
+    def response_callback(self):
+        pass
+    
     def optitrack_callback(self, msg):
         self.curr_measured_x = msg.position.x
         self.curr_measured_y = msg.position.y
@@ -116,13 +160,95 @@ class ControlActionClient(Node):
     def get_result_callback(self, future):
         result = future.result().result
         print(f"Number of points: {result.number_of_points}")
+        self.number_of_points = result.number_of_points
         self.x_arr = result.x
         self.y_arr = result.y
         self.z_arr = result.z
-        print(self.x_arr)
+        print(len(self.x_arr))
+        print(len(self.y_arr))
+        print(len(self.z_arr))
         # self.get_logger().info('Result: f'.format(result.x))
-        rclpy.shutdown()
+        # rclpy.shutdown()
+        time.sleep(5)
+        self.start_control = True
         
+    def timer_callback(self):
+        if self.new_cmd:
+            self.send_drone_cmd()
+            self.new_cmd = False
+        else:
+            self.twist_cmd = Twist()
+            self.send_drone_cmd()
+            
+    def control_timer_callback(self):
+        if self.start_control:
+            self.reached_x = False
+            self.reached_y = False
+            self.reached_z = False
+            if self.curr_point < self.number_of_points - 1:
+                print('point:', self.curr_point, '/', self.number_of_points)
+                self.new_cmd = True
+                self.reached_x = self.set_x_velocity(self.x_arr[self.curr_point])
+                self.reached_y = self.set_y_velocity(self.y_arr[self.curr_point])
+                self.reached_z = self.set_z_velocity(self.z_arr[self.curr_point])
+            else:
+                print('final point')
+                self.twist_cmd = Twist()
+                self.new_cmd = True
+                # time.sleep(2)
+                # rclpy.shutdown()
+            
+            if self.reached_x and self.reached_y and self.reached_z:
+                self.curr_point += 1
+                self.twist_cmd = Twist()
+    
+    def set_x_velocity(self, dest_x):
+        self.curr_x = self.curr_measured_x
+        if (dest_x - self.lin_offset) < self.curr_x < (dest_x + self.lin_offset):
+            self.twist_cmd.linear.x = 0.0
+            return True
+        elif (dest_x - self.lin_offset) > self.curr_x:
+            self.twist_cmd.linear.x = self.vel_lin
+            return False
+        elif (dest_x + self.lin_offset) < self.curr_x:
+            self.twist_cmd.linear.x = -self.vel_lin
+            return False
+            
+    def set_y_velocity(self, dest_y):
+        self.curr_y = self.curr_measured_y
+        if self.curr_y - self.lin_offset < dest_y < self.curr_y + self.lin_offset:
+            self.twist_cmd.linear.y = 0.0
+            return True
+        elif self.curr_y - self.lin_offset > dest_y:
+            self.twist_cmd.linear.y = -self.vel_lin
+            return False
+        elif self.curr_y + self.lin_offset < dest_y:
+            self.twist_cmd.linear.y = self.vel_lin
+            return False
+            
+    def set_z_velocity(self, dest_z):
+        self.curr_z = self.curr_measured_z
+        if self.curr_z - self.lin_offset < dest_z < self.curr_z + self.lin_offset:
+            self.twist_cmd.linear.z = 0.0
+            return True
+        elif self.curr_z - self.lin_offset > dest_z:
+            self.twist_cmd.linear.z = -self.vel_lin
+            return False
+        elif self.curr_z + self.lin_offset < dest_z:
+            self.twist_cmd.linear.z = self.vel_lin
+            return False
+        
+    def send_drone_cmd(self):
+        if self.control_method == True:
+            multi = 10
+            srv_req = TelloAction.Request()
+            srv_req.cmd = f"rc {int(self.twist_cmd.linear.x*multi)} {int(self.twist_cmd.linear.y*multi)} {int(self.twist_cmd.linear.z*multi)} {int(-1*self.twist_cmd.angular.z*30 )}"
+            self._client_rc.call_async(srv_req)
+            self.get_logger().info(f"{srv_req.cmd}")
+        else:
+            self._pub_cmd_vel.publish(self.twist_cmd)
+            self.get_logger().debug(f"{self.twist_cmd}")
+    
     def calc_pid_x(self):
         self.current_time = time.time() - self.last_time
         
